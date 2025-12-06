ca40781c207a90ea8251d56dcdf48ea3bec68aac
Kevin <kevin@kevin.com>
2025-12-06 16:18:14 -0500

    Meilleur saut et sprint

--- personnage_principal.gd
+++ personnage_principal.gd
@@ -11,9 +11,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Saut") and is_on_floor():
+	if Input.is_action_pressed("Saut") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("Sprint"):
+	if Input.is_action_pressed("Sprint"):
 		SPEED = 10.0
 	else:
 		SPEED = 5.0
