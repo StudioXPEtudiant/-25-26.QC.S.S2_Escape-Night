f8daf69c2bfcf34f3574229831f450290505f8b0
Kevin <kevin@kevin.com>
2025-12-06 15:38:35 -0500

    re sprint

--- personnage_principal.gd
+++ personnage_principal.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D 
 
 
-const SPEED = 5.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
@@ -13,7 +13,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Saut") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("Sprint"):
+		SPEED = 10.0
+	else:
+		SPEED = 5.0
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Gauche", "Droite", "Avance", "Recule")
@@ -25,4 +28,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+
+	
 	move_and_slide()
