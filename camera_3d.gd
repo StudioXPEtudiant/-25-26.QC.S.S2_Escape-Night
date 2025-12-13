6443aaddd9ab8051701aae41cb59f4d97b5342a3
legoevolution <legoevolution83@gmail.com>
2025-12-13 15:05:48 -0500

    ok

---
+++ camera_3d.gd
@@ -0,0 +1,12 @@
+extends Camera3D
+
+var FOLLOWER 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass 
+
+
+
+func _process(delta: float) -> void:
+	
+	pass
--- monstreIA.gd
+++ monstreIA.gd
@@ -1,7 +1,7 @@
-extends CharacterBody3D 
+extends CharacterBody3D
 
 
-var SPEED = 5.0
+const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
@@ -11,15 +11,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("Saut") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	if Input.is_action_pressed("Sprint"):
-		SPEED = 10.0
-	else:
-		SPEED = 5.0
-	
-	
-	var input_dir := Input.get_vector("Gauche", "Droite", "Avance", "Recule")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -28,6 +25,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-
-	
 	move_and_slide()
