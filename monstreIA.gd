a03b2b02e521ab119c735672a6fb80537ec19526
Kevin <kevin@kevin.com>
2026-01-31 15:32:29 -0500

    hello

--- monstreIA.gd
+++ monstreIA.gd
@@ -1,28 +1,20 @@
 extends CharacterBody3D
 
+@onready var nav_agent = $NavigationAgent3D
+@export var target_node: Node3D # Glissez la cible ici
+@export var speed = 5.0
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+func _physics_process(delta):
+	if target_node:
+		# Mettre à jour la cible de l'agent
+		nav_agent.target_position = target_node.global_position
+	
+	if nav_agent.is_navigation_finished():
+		return
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	# Obtenir la prochaine position sur le chemin
+	var next_path_pos = nav_agent.get_next_path_position()
+	var new_velocity = global_position.direction_to(next_path_pos) * speed
+	
+	velocity = new_velocity
 	move_and_slide()
