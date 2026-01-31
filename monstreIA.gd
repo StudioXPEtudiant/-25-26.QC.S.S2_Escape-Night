2be4735dea0a6205b5d3d509d4fe59ab6aad0c37
Kevin <kevin@kevin.com>
2026-01-31 16:01:11 -0500

    qqch

--- monstreIA.gd
+++ monstreIA.gd
@@ -6,13 +6,11 @@ extends CharacterBody3D
 
 func _physics_process(delta):
 	if target_node:
-		# Mettre à jour la cible de l'agent
 		nav_agent.target_position = target_node.global_position
 	
 	if nav_agent.is_navigation_finished():
 		return
 
-	# Obtenir la prochaine position sur le chemin
 	var next_path_pos = nav_agent.get_next_path_position()
 	var new_velocity = global_position.direction_to(next_path_pos) * speed
 	
