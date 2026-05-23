bfcec828980a47e8ef1057e4278ea744588ec153
Kevin <kevin@kevin.com>
2026-05-23 16:00:11 -0400

    Replasage

--- script/camera_3d.gd
+++ script/camera_3d.gd
@@ -5,25 +5,15 @@ var angle_cam = rotation_degrees.y
 #@export var sens_souris: float = 1e-2# sensibilité de la cam
 func _process(delta: float) -> void:
 	Input.get_last_mouse_velocity()
-	#rotation.y = clamp(rotation.y, deg_to_rad(-80), deg_to_rad(80))
-	#rotation.x = clamp(rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 func _input(event):
-	pass
 	if event is InputEventMouseMotion:
 		var mouse_delta = event.relative
 		rotate_x(-event.relative.y * 0.1)
-		rotation.x = clamp(rotation.x, deg_to_rad(-80), deg_to_rad(80))
-	#if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		# Rotation horizontale (autour de Y)
-	#	rotation.y -= event.relative.x * sens_souris
-		
-		# Rotation verticale (autour de X)
-		#rotation.x -= event.relative.y * sens_souris
-		
-		# Limiter la rotation verticale pour éviter les retournements (ex: entre -80 et 80 degrés)
-	#	rotation.x = clamp(rotation.x, deg_to_rad(-80), deg_to_rad(80))
-
+		rotate_y(-event.relative.x * 0.1)
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotation.x = clamp(rotation.x, deg_to_rad(-60), deg_to_rad(60))
+		#rotation.y = clamp(rotation.y, deg_to_rad(-60), deg_to_rad(60))
 
 
 func _ready():
@@ -34,5 +24,9 @@ func _unhandled_input(event):
 	# Relâcher le curseur avec la touche Échap
 	if event.is_action_pressed("Cancel"): # "ui_cancel" est souvent la touche Échap
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	#if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		#if event.is_action_pressed("Cancel"):
+			#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	# Rere-capturer si on clique à nouveau (vous pourriez ajouter une logique)
 	
